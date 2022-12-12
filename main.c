4bf89b60f1707be721ab7482001ce15f45e422bb
LucasVianaSampaio <lv6612530@gmail.com>
2022-12-11 22:36:54 -0300

    Update main.c

--- main.c
+++ main.c
@@ -33,29 +33,19 @@ int main(int argc, char **argv)
                 printf("erro de alocacao de memoria!");
                 exit(2);
             }
-            // printf(" ->img1:%s\n", dir->d_name);
             c = (int)(*(dir->d_name + 0));
             readPGMImage(&img, dir->d_name);
             quantizar(img.pData, img.c, img.r, img.mv, freq);
 
             dir = readdir(d);
 
-            // printf(" ->img:%s\n", dir->d_name);
             readPGMImage(&img2, dir->d_name);
             quantizar(img2.pData, img2.c, img2.r, img2.mv, freq);
 
             scm(img.pData, img2.pData, mat_scm, img.c, img.r, freq);
 
-            // imprimir_scm(mat_scm, freq);
             writePGMImage(mat_scm, argv[2], freq, &c);
             cont += 2;
-            // printf("________________________________________________\n");
-
-            // Leitura da Imagem -PGM
-
-            // Processar - Medir o tempo apenas nesta etapa.
-
-            // Saída.
             free(mat_scm);
             fim = clock();
             soma = (float)(fim - ini) / CLOCKS_PER_SEC;
